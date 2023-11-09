2a91ca9fb483504dc93f566d43c0f87583ade9ff
Muris Nuhodzic <muris.nuhodzic@blueface.com>
2023-11-09 10:17:10 +0100

    Updated dependencies

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './view';
 import icons from '../../img/icons.svg';
 // const Fraction = require('fractional').Fraction;
-import { Fraction } from 'fractional';
+import * as fracty from 'fracty';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -80,7 +80,7 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${
-            ing.quantity ? new Fraction(ing.quantity).toString() : ''
+            ing.quantity ? String(fracty(ing.quantity)) : ''
           }</div>
           <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
